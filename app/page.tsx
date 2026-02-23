d8c1902e0d2685964b3a9230613e0e68ed575792
chris-lawbrokr <chris@lawbrokr.com>
2026-02-23 12:31:30 -0500

    Initial storybook config

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>Test</main>;
+  return (
+    <main>
+      <Button>click</Button>
+    </main>
+  );
 }
