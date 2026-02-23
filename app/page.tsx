7450a9a5972c8f29ec91f5895d766d07e44a2f0b
chris-lawbrokr <chris@lawbrokr.com>
2026-02-23 10:54:42 -0500

    Revert "Initial card and button components"
    This reverts commit 6493fd18ee4562992113efc810fb3c25b9d485e9.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button/button";
-
-export default function Home() {
-  return (
-    <main className="h-screen w-screen flex items-center justify-center">
-      <Button>Click</Button>
-    </main>
-  );
-}
