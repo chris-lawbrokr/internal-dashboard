598fd1cb4149c70089404d6cb833387ff9f415ef
chris-lawbrokr <chris@lawbrokr.com>
2026-03-31 10:32:38 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useAuth } from "@/lib/auth";
-import { PageHeader } from "@/components/ui/page-header/PageHeader";
 
 import { Card } from "@/components/ui/card";
+import { PageHeader } from "@/components/ui/page-header/PageHeader";
 
 export default function Home() {
   const { user } = useAuth();
   const firstname = user?.first_name ?? "";
+
   return (
     <div className="w-full h-full flex flex-col gap-4">
       <PageHeader title={`Welcome back, ${firstname}`} back="/accounts" />
